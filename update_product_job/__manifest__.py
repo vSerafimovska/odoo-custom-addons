d6e93fd5610b91274fe15228d5340ec21b773249
vSerafimovska <121295274+vSerafimovska@users.noreply.github.com>
2024-11-26 10:47:19 +0100

    Job description added

--- update_product_job/__manifest__.py
+++ update_product_job/__manifest__.py
@@ -1,11 +1,10 @@
 {
-    'name': 'Update Product Job',
-    'version': '0.1',
-    'depends': ['sale', 'queue_job'],
-    'data': [
-        'views/product_template.xml',
+    "name": "Update Product Job",
+    "version": "0.1",
+    "depends": ["sale", "queue_job"],
+    "data": [
+        "views/product_template.xml",
     ],
-    'installable': True,
-    'application': False,
-
+    "installable": True,
+    "application": False,
 }
